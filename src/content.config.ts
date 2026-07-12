56ac433b352d793f615822f888a9266b8c49da4e
zhigiow <waizhigiow2003@gmail.com>
2026-07-12 16:02:14 +0800

    修复了博客排序并增加了index属性

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			index: z.number().optional(),
 		}),
 });
 
